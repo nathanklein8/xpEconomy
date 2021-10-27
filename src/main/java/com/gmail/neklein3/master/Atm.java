c3a4397e6864d98c2364a7def95797e9e497325e
Nathan Klein <neklein3@gmail.com>
2021-10-27 19:54:08 -0400

    kept your config stuff, it's commented out.  added different config stuff and methods to go along with the configs

--- src/main/java/com/gmail/neklein3/master/Atm.java
+++ src/main/java/com/gmail/neklein3/master/Atm.java
@@ -1,22 +1,39 @@
 package com.gmail.neklein3.master;
 
-import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Sign;
+import org.bukkit.block.data.type.WallSign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.SignChangeEvent;
 
 public class Atm implements Listener {
 
-    private Main main;
-    public Atm(Main main) { this.main = main; }
+    Main main;
+    Atm(Main main) { this.main = main; }
 
     //atm creation.
     @EventHandler
     public void onSignWrite(SignChangeEvent e) {
-        Player player = e.getPlayer();
+        Block block = e.getBlock();
+
         if(e.getLine(0) != null && e.getLine(0).equals("[atm]")) {
-
+            //logs the sign location in atmLocations in the config
+            main.addATMLocation(block.getLocation());
+            main.getLogger().info("placed atm");
         }
+    }
 
+    @EventHandler
+    public void onBreakSign(BlockBreakEvent e) {
+        Block block = e.getBlock();
+
+        // remove the location of the sign from the atmLocation list if that location was already in the list
+        if (block.getBlockData() instanceof Sign || block.getBlockData() instanceof WallSign) {
+            main.getLogger().info("mined atm");
+            main.removeATMLocation(block.getLocation());
+        }
     }
 }
--- src/main/java/com/gmail/neklein3/master/Main.java
+++ src/main/java/com/gmail/neklein3/master/Main.java
@@ -2,17 +2,43 @@ package com.gmail.neklein3.master;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main extends JavaPlugin implements Listener {
 
-    private File atmFile;
-    private YamlConfiguration modifyAtmFile;
+//    private File atmFile;
+//    private YamlConfiguration modifyAtmFile;
+
+    FileConfiguration config = getConfig();
+
+    ArrayList<Location> atmLocations = new ArrayList<>();
+
+    public void addATMLocation(Location location) {
+        // adds location to atm locations if it's not already in the list
+        if (!(atmLocations.contains(location))) {
+            atmLocations.add(location);
+            config.set("atmLocations", atmLocations);
+            saveConfig();
+        }
+    }
+
+    public void removeATMLocation(Location location) {
+        // removes location from the list if it is an atm location
+        if (atmLocations.contains(location)) {
+            atmLocations.remove(location);
+            config.set("atmLocations", atmLocations);
+            saveConfig();
+        }
+    }
+
+    public boolean isATMLocation(Location location) {
+        return atmLocations.contains(location);
+    }
 
     @Override
     public void onEnable() {
@@ -37,6 +63,10 @@ public class Main extends JavaPlugin implements Listener {
         this.getCommand("spawnEmployer").setExecutor(new EmployerCommand());
         this.getCommand("giveMoneyXp").setExecutor(new LevelUpCommand(this));
         getLogger().info("commands have been added");
+
+        config.addDefault("atmLocations", atmLocations);
+        saveConfig();
+
     }
 
     @Override
@@ -44,19 +74,19 @@ public class Main extends JavaPlugin implements Listener {
 
     }
 
-    public YamlConfiguration modifyAtmFile() { return modifyAtmFile; }
-    public File getAtmFile() { return atmFile; }
-
-    public void initiateFiles() throws IOException {
-        atmFile = new File(Bukkit.getServer().getPluginManager().getPlugin("xpEconomy").getDataFolder(), "atmFile.yml");
-        if (!atmFile.exists()) {
-            atmFile.createNewFile();
-            modifyAtmFile().save(this.getAtmFile());
-        }
-        modifyAtmFile = YamlConfiguration.loadConfiguration(atmFile);
-        modifyAtmFile().save(this.getAtmFile());
-        //end of initiateFiles
-    }
+//    public YamlConfiguration modifyAtmFile() { return modifyAtmFile; }
+//    public File getAtmFile() { return atmFile; }
+//
+//    public void initiateFiles() throws IOException {
+//        atmFile = new File(Bukkit.getServer().getPluginManager().getPlugin("xpEconomy").getDataFolder(), "atmFile.yml");
+//        if (!atmFile.exists()) {
+//            atmFile.createNewFile();
+//            modifyAtmFile().save(this.getAtmFile());
+//        }
+//        modifyAtmFile = YamlConfiguration.loadConfiguration(atmFile);
+//        modifyAtmFile().save(this.getAtmFile());
+//        //end of initiateFiles
+//    }
 
     public String color(final String string) {
         return ChatColor.translateAlternateColorCodes('&', string);
